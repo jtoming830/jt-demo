185c3246078267bcfb1b56b3124d9bfeee08d979
Jaan Toming <jtoming@gmail.com>
2023-06-15 10:35:29 +0300

    test requests

--- client/src/pages/MoviesRates/MoviesRates.jsx
+++ client/src/pages/MoviesRates/MoviesRates.jsx
@@ -1,5 +1,11 @@
+import { useEffect } from 'react'
 import { Table } from './components'
+import { makeRequest } from '../../api/makeRequest'
 
 export default function MoviesRates() {
+  useEffect(() => {
+    makeRequest('/test', { method: 'POST' }).then(console.log).catch(console.log)
+  }, [])
+
   return <Table />
 }
--- service/src/app.controller.ts
+++ service/src/app.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Post('/test')
+  test() {
+    return this.appService.getHello();
+  }
 }
