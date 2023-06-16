dfa73ab8c5e379d1c56d84827f4775414e27e23e
Jaan Toming <jtoming@gmail.com>
2023-06-16 04:05:51 +0300

    add links

--- clients/cv/src/pages/DemoGallery/DemoGallery.jsx
+++ clients/cv/src/pages/DemoGallery/DemoGallery.jsx
@@ -1,3 +1,26 @@
+import { Link } from 'react-router-dom'
+
 export default function DemoGallery() {
-  return <div>DEMO GALLERY</div>
+  const { origin } = document.location
+
+  return (
+    <>
+      <div>
+        <Link
+          to={`${origin}/movies`}
+          reloadDocument
+        >
+          Movies Scores
+        </Link>
+      </div>
+      <div>
+        <Link
+          to={`${origin}/sample_landing`}
+          reloadDocument
+        >
+          Sample Landing
+        </Link>
+      </div>
+    </>
+  )
 }
