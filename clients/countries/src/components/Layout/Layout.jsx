1d9192feaddd7e45bcfc7acb25ba5b4c66b5ec48
Jaan Toming <jtoming@gmail.com>
2023-06-18 22:07:16 +0300

    add bottom margin for countries layout

--- clients/countries/src/components/Layout/Layout.jsx
+++ clients/countries/src/components/Layout/Layout.jsx
@@ -3,7 +3,7 @@ import { Header } from './Header'
 import { styled } from 'styled-components'
 
 const Content = styled.div`
-  margin: 0 80px;
+  margin: 0 80px 80px 80px;
 
   @media only screen and (max-width: 768px) {
     margin: 0 16px;
